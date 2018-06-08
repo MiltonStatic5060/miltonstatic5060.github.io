42df9b791c98f22ef585762737905ad07d396624
Ryan Au <auryan898@gmail.com>
2018-06-08 03:49:45 -0400

    Implemented Vue js to start adding modularity to the site

---
+++ main/vue_module.js
@@ -0,0 +1,25 @@
+
+Vue.component('vue-nav-menu', {
+    props: ['buttons'],
+    template:
+`
+`
+}) 
+Vue.component('vue-nav-button', {
+    props: ['name','classes','link'],
+    template: 
+`<li :class="classes" style="border-radius:5px;">
+    <a class="nav-link js-scroll-trigger" style="color:white;border-radius:5px;" :href="link">{{name}}</a>
+</li>`, 
+})
+
+var vue_nav_menu = new Vue({
+    el:"#vue_nav_menu",
+    data: {
+        buttons: [
+            {name:"Members",link:"./members.html",classes:"nav-item bg-info"},
+            {name:"Robot Cheat Sheet", link:"./gamepad.html", classes:"nav-item bg-warning"},
+            {name:"Programming Tutorial", link:"/5060-tutorials", classes:"nav-item bg-success"}
+        ]
+    }
+});
